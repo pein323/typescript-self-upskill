b20765ee31a7fe910b992cdb0e7367f19f50c844
Krzysztof Bistram <bistram.k@gmail.com>
2024-10-04 14:08:08 +0200

    Challenge one - 1

---
+++ src/challenge-one.ts
@@ -0,0 +1,51 @@
+export interface DataEntity {
+  id: string;
+}
+export interface Movie extends DataEntity {
+  director: string;
+  addMovie: ({ id, director }) => void;
+  getMovie: (id: string) => Movie;
+  getAllMovies: () => Movie[];
+  clearMovies: () => void;
+}
+export interface Song extends DataEntity {
+  singer: string;
+
+  addSong: ({ id, singer }) => void;
+  getSong: (id: string) => Song;
+  getAllSongs: () => Song[];
+  clearSongs: () => void;
+}
+
+export type DataEntityMap = {
+  movie: Movie;
+  song: Song;
+};
+
+export class DataStore implements Movie, Song {
+  addSong: ({ id, singer }: { id: any; singer: any }) => void;
+  getSong: (id: string) => Song;
+  getAllSongs: () => Song[];
+  clearSongs: () => void;
+  addMovie: ({ id, director }: { id: any; director: any }) => void;
+  getMovie: (id: string) => Movie;
+  getAllMovies: () => Movie[];
+  clearMovies: () => void;
+
+  director: string;
+  id: string;
+  singer: string;
+}
+
+const ds = new DataStore();
+ds.addSong({ id: "song-123", singer: "The Flaming Lips" });
+ds.addMovie({
+  id: "movie-456",
+  director: "Stephen Spielberg",
+});
+ds.getSong("song-123"); // returns the song
+ds.getMovie("movie-456"); // returns the movie
+ds.getAllSongs(); // array of all songs
+ds.getAllMovies(); // array of all movies
+ds.clearSongs(); // clears all songs
+ds.clearMovies(); // clears all movies
